aacf73a8da06970de14a04ea0355674a04dfb50f
holyseo <128103221+holyseo@users.noreply.github.com>
2024-07-11 14:46:19 +0100

    update currency

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,9 +19,9 @@ const ProductCard = ({ item }: ProductCardProps) => {
   return (
     <div
       key={item.id}
-      className=" max-w-xs max-h-96 p-4 flex flex-col items-center gap-5 border border-slate-200 shadow-lg"
+      className="flex flex-col items-center max-w-xs gap-5 p-4 border shadow-lg  max-h-96 border-slate-200"
     >
-      <div className=" w-52 h-36 md:w-60 md:h-48 relative">
+      <div className="relative  w-52 h-36 md:w-60 md:h-48">
         <Image
           src={item.image}
           alt={item.title}
@@ -29,7 +29,7 @@ const ProductCard = ({ item }: ProductCardProps) => {
           style={{ objectFit: "contain" }}
         />
       </div>
-      <div className="text-sm font-medium flex justify-between w-full ">
+      <div className="flex justify-between w-full text-sm font-medium ">
         <p className="has-tooltip">
           <span className="tooltip bg-[#333333e6] text-white px-2 py-1">
             {item.title}
@@ -38,18 +38,18 @@ const ProductCard = ({ item }: ProductCardProps) => {
             ? item.title.substring(0, 25) + "..."
             : item.title}
         </p>
-        <p className="text-gray-600">${item.price}</p>
+        <p className="text-gray-600">£{item.price}</p>
       </div>
       <div className="text-[10px] font-medium flex gap-8 items-center w-full">
         <div className="flex has-tooltip">
-          <span className="tooltip px-1 mt-1">Rating: {item.rating.rate}</span>
+          <span className="px-1 mt-1 tooltip">Rating: {item.rating.rate}</span>
           {stars}
         </div>
         <p>from {item.rating.count} reviews</p>
       </div>
       <Link
         href={`/product/${item.id}`}
-        className="bg-black text-white text-center font-medium text-sm w-full p-2"
+        className="w-full p-2 text-sm font-medium text-center text-white bg-black"
       >
         VIEW PRODUCT
       </Link>
